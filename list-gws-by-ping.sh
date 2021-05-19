a75916c7c2e1c5a83d1d08094586e85f4680db19
rany <ranygh@riseup.net>
2021-05-19 12:52:47 +0300

    Add list gateway based on ping

---
+++ list-gws-by-ping.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+( echo 'Average Latency (ms)|Country|Server Address'
+for x in $(./riseupvpn.py -l | awk '{print $1"+"$NF}')
+do
+echo "$(ping -c4 -i0.2 "$(cut -d+ -f2 <<<"$x")" |tail -1 |cut -d/ -f5|sed -e 's/^$/-1/g')|$(cut -d+ -f1 <<<"$x")|$(cut -d+ -f2<<<"$x")" &
+done | sort -n ) | column -s '|' -t
